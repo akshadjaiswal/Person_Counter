cabffb114ff32e51cacf12ac7263f3cfc66e310c
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-23 10:58:35 +0530

    Save function

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+let saveEl=document.getElementById("save-el")
 let incrementCounter = document.getElementById("count");
 let count = 0;
 
 //Increment counter
-increment = () => {
-  count = count + 1;
+function increment () {
+  count +=1;
   incrementCounter.innerText = count;
 };
 
-save = () => {
-  console.log(count);
+function save  () {
+  let countStr = " "+count + " - "
+  saveEl.innerText += countStr;
+  
 };
-save();
+
