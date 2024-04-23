17311f1b5b5e9a04cb9585925fc2463984c8f32e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-04-23 11:35:13 +0530

    Reset function

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-let saveEl=document.getElementById("save-el")
+let saveEl = document.getElementById("save-el");
 let incrementCounter = document.getElementById("count");
+let resetBtn = document.getElementById("reset-btn");
 let count = 0;
 
 //Increment counter
-function increment () {
-  count +=1;
+function increment() {
+  count += 1;
   incrementCounter.innerText = count;
-};
+}
 
-function save  () {
-  let countStr = " "+count + " - "
-  saveEl.innerText += countStr;
-  
-};
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+}
 
+function reset() {
+  incrementCounter.textContent = 0;
+  count=0
+}
